621c56f5e1b00c308c5e65cb4b796b8f8e92ec59
necho1122 <nelsonubac90@gmail.com>
2025-06-12 22:13:12 -0300

    add 5th project

---
+++ 5_barra-lateral/index.js
@@ -0,0 +1,12 @@
+const addClassToUl = document.getElementById('lateral-bar');
+const hideBtn = document.getElementById('show-menu-btn');
+
+function showMenu() {
+	addClassToUl.classList.add('activo', 'menu-list');
+	//hideBtn.classList.add('btn-hide', 'bton-disable');
+}
+
+function hideMenu() {
+	addClassToUl.classList.remove('activo', 'menu-list');
+	hideBtn.classList.remove('btn-hide', 'bton-disable');
+}
